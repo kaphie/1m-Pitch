ab1063670f889ccad2269e340654c9cefdfa5970
kaphie <bkafrika144@gmail.com>
2020-05-04 10:43:11 +0300

    adding content to init.py

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+
+from config import Config
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+bootstrap = Bootstrap(app)
+mail = Mail(app)
+login_manager.login_view = 'auth.login'
+login_manager.session_protection = 'strong'
+
+
+def create_app():
+    app.config.from_object(Config)
+    from .auth import auth as auth_blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(main_blueprint)
+    return app
