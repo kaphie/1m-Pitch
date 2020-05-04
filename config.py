feb3ff95dad4ed185236aaaebe79220c441444fb
kaphie <bkafrika144@gmail.com>
2020-05-04 10:33:37 +0300

    adding content to config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv as ld
+
+ld()
+
+
+class Config:
+    debug = True
+    SECRET_KEY = 'qwertyuiop'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+
